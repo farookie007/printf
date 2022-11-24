0e24f06c6cc59755103dabe6adc6682f567fe723
farookie007 <faroukadekunle001@gmail.com>
2022-11-24 20:23:40 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+
+void test_print(char *str, ...);
+
+/**
+ * main - test file
+ * Program starts execution from here
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	/* Tests for the printf standard library */
+	write(1, "STANDARD:\n", 10);
+	printf("Tester\n");
+
+	/* Tests for the custom made printf function */
+	write(1, "CUSTOM MADE:\n", 13);
+	_printf("Tester\n");
+
+	return (0);
+}
